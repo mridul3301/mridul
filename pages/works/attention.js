1a19091ac7411804a03955b44c550d2bd2d9dd14
Mridul Sharma <mridulsharma3301@gmail.com>
2023-07-04 20:10:18 +0545

    updated

--- pages/works/attention.js
+++ pages/works/attention.js
@@ -19,12 +19,12 @@ const Work = () => (
       </Heading>
       <P style={{ width: '750px', textAlign: 'left' }}>
       
-      The "Attention is All You Need" paper, published in 2017 by Vaswani et al., introduced
+      <span>The "Attention is All You Need" paper, published in 2017 by Vaswani et al., introduced
       the Transformer model, a revolutionary architecture for sequence transduction tasks, such 
       as machine translation. The paper presented a new approach to eliminate recurrent neural 
       networks (RNNs) and convolutional neural networks (CNNs) in favor of a purely attention-based 
       model. This approach achieved state-of-the-art results on machine translation benchmarks with 
-      significantly faster training times.<br /><br />
+      significantly faster training times.</span><br /><br />
       </P>
       <Heading as="h4" fontSize={14} my={6}>
         <Center>Key Concepts :<br /></Center>
@@ -35,61 +35,61 @@ const Work = () => (
           <Meta>1. Attention Mechanism :<br /></Meta>
           <P style={{ width: '750px', textAlign: 'left' }}>
       
-          The paper introduces the attention mechanism as a fundamental building block for sequence 
+          <span>The paper introduces the attention mechanism as a fundamental building block for sequence 
           modeling. Attention allows the model to focus on different parts of the input sequence when 
           making predictions. It computes attention weights that represent the relevance or importance 
-          of different elements in the input sequence.<br /><br />
+          of different elements in the input sequence.</span><br /><br />
           </P>
         </ListItem>
         <ListItem>
           <Meta>2. Transformer Architecture :<br /></Meta>
           <P style={{ width: '750px', textAlign: 'left' }}>
       
-          The authors propose the Transformer, a novel neural network architecture based solely on 
+          <span>The authors propose the Transformer, a novel neural network architecture based solely on 
           attention mechanisms, without any recurrent or convolutional components. The Transformer 
           consists of an encoder and a decoder. Both encoder and decoder are composed of multiple 
           layers, each containing a multi-head self-attention mechanism and a position-wise 
-          feed-forward network.<br /><br />
+          feed-forward network.</span><br /><br />
           </P>
         </ListItem>
         <ListItem>
           <Meta>3. Self Attention :<br /></Meta>
           <P style={{ width: '750px', textAlign: 'left' }}>
       
-          Self-attention is a mechanism that computes attention weights within a sequence itself. 
+          <span>Self-attention is a mechanism that computes attention weights within a sequence itself. 
           In the Transformer, self-attention is employed to model dependencies between different 
           positions within the input sequence. It allows the model to attend to all positions 
-          simultaneously, capturing both short-range and long-range dependencies.<br /><br />
+          simultaneously, capturing both short-range and long-range dependencies.</span><br /><br />
           </P>
         </ListItem>
         <ListItem>
           <Meta>4. Multi Head Attention :<br /></Meta>
           <P style={{ width: '750px', textAlign: 'left' }}>
       
-          To enhance the modeling capacity and enable capturing different types of dependencies, the 
+          <span>To enhance the modeling capacity and enable capturing different types of dependencies, the 
           authors introduce multi-head attention. This mechanism performs multiple parallel 
           self-attention operations with different learned linear projections, allowing the model 
-          to jointly attend to information from different representation subspaces.<br /><br />
+          to jointly attend to information from different representation subspaces.</span><br /><br />
           </P>
         </ListItem>
         <ListItem>
           <Meta>5. Positional Encoding :<br /></Meta>
           <P style={{ width: '750px', textAlign: 'left' }}>
       
-          Since the Transformer lacks sequential information, positional encodings are introduced to 
+          <span>Since the Transformer lacks sequential information, positional encodings are introduced to 
           provide the model with information about the order of elements in the sequence. These 
           positional encodings are added to the input embeddings and provide the model with a sense 
-          of sequence order.<br /><br />
+          of sequence order.</span><br /><br />
           </P>
         </ListItem>
         <ListItem>
           <Meta>6. Training and Inference :<br /></Meta>
           <P style={{ width: '750px', textAlign: 'left' }}>
       
-          The paper describes the training process for the Transformer using the attention-based 
+          <span>The paper describes the training process for the Transformer using the attention-based 
           sequence-to-sequence framework. During training, the model is optimized with respect to 
           a maximum likelihood objective. During inference, the model generates translations 
-          autoregressively, where each output token is generated based on previously generated tokens.<br /><br />
+          autoregressively, where each output token is generated based on previously generated tokens.</span><br /><br />
           </P>
         </ListItem>
 
--- pages/works/mle.js
+++ pages/works/mle.js
@@ -16,10 +16,11 @@ const Work = () => (
         <Center>Concept</Center>
       </Heading>
       <P style={{ width: '750px', textAlign: 'left' }}>
-      Let's say we have some data,<br />
+      <span>Let's say we have some data,<br />
       The goal of maximum likelihood is to find the optimal way to fit a desired distribution to the data.<br />
       <br />
-      The reason behind fitting a distribution to our data is that it can be easier to work with the data and it is also more general i.e. applies to every experiment of same type.
+      The reason behind fitting a distribution to our data is that it can be easier to work with 
+      the data and it is also more general i.e. applies to every experiment of same type.</span>
       </P>
       <P>
       </P>
